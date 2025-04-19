3b0011bbe41cf3962f039790f28aae5008fac645
Felizitas Zoi Loske <43696085+Felos259@users.noreply.github.com>
2025-04-19 13:18:23 +0200

    cudos to myself last week

---
+++ M12/GrundfrequenzLaenge.py
@@ -0,0 +1,85 @@
+import numpy as np  # Numpy array library
+import pandas as pd # Pandas Dataframes um Messdaten zu speichern
+import matplotlib.pyplot as plt # Plots erstellen
+import matplotlib.mlab as mlab
+import scipy.stats # Statistische Auswertung - Unsicherheiten werden automatisch eingerechnet
+from scipy.optimize import curve_fit
+import iminuit as i
+import uncertainties as u
+import uncertainties.umath as um
+from uncertainties import unumpy as unp
+
+RF = pd.read_csv('GrundfrequenzLaenge.csv', header=2, sep=';')
+
+uL = unp.uarray(RF['Laenge'], RF['dL'])
+uf =unp.uarray(RF['Grundfrequenz'], RF['df'])
+
+uM = u.ufloat(1,0.007)
+
+# 1 durch f bestimmen
+durchf = 1 / uf
+
+# Wert und Unsichheit in Dataset einlesen
+RF['durchf']=np.array([value.nominal_value for value in durchf])
+RF['deldurchf']=np.array([value.s for value in durchf])
+
+# Figure und Subplots erstellen - bei denen alle Subplots die gleichen Achsen haben
+fig, ax = plt.subplots()
+# fig ist das eigentliche Bild, ax ist ein Datenobjeke
+
+# Achsen richten
+ax.set_xlim(0,0.69999)
+ax.set_ylim(0, 105)
+
+# Plot der Messwerte L und 1/f mit Errorbars 
+ax.errorbar(RF.Laenge, RF.durchf, xerr=RF.dL , yerr=RF.deldurchf, label='$\\frac{1}{f}$ in Abhängigkeit der Saitenlänge', color = 'lightblue', linestyle='None', marker='o', capsize=6)
+
+# linearer Fit
+
+# Fitfunktion definieren
+def fit_function(x, A):
+    return A * x
+
+#Daten
+x_data = RF['Laenge']
+y_data = RF['durchf']
+y_err = RF['deldurchf'] 
+
+# Curve-Fit mit Unsicherheiten in y
+params, covariance = curve_fit(fit_function, x_data, y_data, sigma=y_err, absolute_sigma=True)
+A_value = params[0]
+fit_errors = np.sqrt(np.diag(covariance))  # Fehler der Fit-Parameter
+A_error = fit_errors[0]
+
+dof = len(RF.index)-len(params)
+chi2 = sum([(fit_function(x,A_value)-y)*2/u*2 for x,y,u in zip(x_data,y_data,y_err)])
+
+# Fit-Ergebnisse ausgeben
+#print(f"A = {A_value:.6f} ± {A_error:.6f}")
+#print(f"x0 = {x0:.6f} ± {x0_error:.6f}")
+#print(f"Chi-Quadrat/dof: {chi2/dof}")
+
+x_ax=np.linspace(0, 10, 1000) 
+y_ax = fit_function(x_ax, A_value)
+
+# Plot zeichnen
+plt.plot(x_ax, y_ax, label=f"Fit: $y = A \\cdot x$ \n $A = {A_value:.6f} \\pm {A_error:.6f}$", linewidth=2, color='blue')
+
+plt.xlabel('Saitenlänge $L$ in m')
+plt.ylabel("$\\frac{1}{f}$ in s")
+plt.legend()
+plt.title("$L$-$\\frac{1}{f}$-Diagramm")
+
+plt.savefig("FrequenzLaenge.pdf", format='pdf', bbox_inches='tight', pad_inches=0.5) 
+plt.savefig("FrequenzLaenge.svg", format='svg', bbox_inches='tight', pad_inches=0.5) 
+
+#plt.show() 
+
+########################################
+
+# keine Ahnung was man beim funktionalen Zusammenhang machen muss
+
+########################################
+
+# Phasengeschwindigkeiten berechnen
+uC =  uf*2.0*uL
---
+++ M12/GrundfrequenzSpannung.py
@@ -0,0 +1,28 @@
+import numpy as np  # Numpy array library
+import pandas as pd # Pandas Dataframes um Messdaten zu speichern
+import matplotlib.pyplot as plt # Plots erstellen
+import matplotlib.mlab as mlab
+import scipy.stats # Statistische Auswertung - Unsicherheiten werden automatisch eingerechnet
+from scipy.optimize import curve_fit
+import iminuit as i
+import uncertainties as u
+import uncertainties.umath as um
+from uncertainties import unumpy as unp
+
+#WICHTIGE ÄNDERUNGEN ZU MACHEN: Unsicherheiten fixen
+
+RF = pd.read_csv('GrundfrequenzSpannung.csv', header=3, sep=';')
+
+uL = u.ufloat(0.6, 0.006)  #UNSICHERHEIT FIXEN
+uM = unp.uarray(RF['Masse'], RF['dM'])
+
+# Massendichte mü berechnen
+umu = uM / uL 
+
+# Zugspannungskraft F_0 berechnen - F_0 = i*M*g + F_LH, i = Kerbe
+
+F_LH = 0.52 # in Anleitung gegeben
+g = u.ufloat(9.812669,0) #https://www.ptb.de/cms/ptb/fachabteilungen/abt1/fb-11/fb-11-sis/g-extractor.html
+
+F_0 = RF['Kerbe'] * uM *g + F_LH
+
