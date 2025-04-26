6a09a26a37b3da2aaa28e675c80224efd96396b8
felos259 <felos259@gmail.com>
2025-04-26 18:25:40 +0200

    ich habe auch mal was gemacht

--- E12/TeilA.py
+++ E12/TeilA.py
@@ -11,74 +11,83 @@ from uncertainties import unumpy as unp
 
 # Unscicherheiten anpassen k, uU_A
 
-mu_0 = 4*np.pi*10**(-7)
-N = 320
-R = 0.068
+# mu_0 = 4*np.pi*10**(-7)
+# N = 320
+# R = 0.068
 
-k = u.ufloat(mu_0 *N/(2*R)*(4/5)**(3/2),0)
+# k = u.ufloat(mu_0 *N/(2*R)*(4/5)**(3/2),0)
 
+################################
 
-RF = pd.read_csv('E12/TeilA3kV.csv', header=0, sep=';')
-
-ue = unp.uarray(RF['e'], RF['de'])
-uI = unp.uarray(RF['I'], RF['dI'])
-uU_A = unp.uarray(3000, 10) # Anodenspannung
-KL = u.ufloat(0.08, 0.0005) # Kantenlänge Schir,
-
-#aus den Messwerten Magnetfeldstärke berechnen
-uB = k*uI
-
-#aus den Messwerten und der Magnetfeldstärke r(B) berechnen
-
-
-
-# Figure und Subplots erstellen - bei denen alle Subplots die gleichen Achsen haben
-#Daten
-x_data = RF['I']
-x_err = RF['dI']
-y_data = RF['B']
-y_err = RF['dB'] 
 
 fig, ax = plt.subplots()
 # fig ist das eigentliche Bild, ax ist ein Datenobjeke
 
 # Achsen richten
-ax.set_xlim(0,0.6999)
-ax.set_ylim(0, 9.999)
+ax.set_xlim(0,1)
+ax.set_ylim(0, 200)
 
-# Plot der Messwerte L und 1/f mit Errorbars 
-ax.errorbar( x_data, y_data, xerr=x_err, yerr=y_err, label='Magnetfeldstärke $B$ in Abhängigkeit der Stromstärke $I$', color = 'lightblue', linestyle='None', marker='o', capsize=6)
+# Datenträger, damit man subplots ansprechen kann - für hübsche Legende
+# Labels und Farben für Achsen speichern 
+label = {'E12/TeilA3kV.csv' : ['$U_A=3$kV', 'Fit zu $U_A=3$kV', 'red', 'orange'],
+         'E12/TeilA4kV.csv' : ['$U_A=4$kV', 'Fit zu $U_A=4$kV', 'blue', 'purple'],
+         'E12/TeilA5kV.csv' : ['$U_A=5$kV', 'Fit zu $U_A=5$kV', 'yellow', 'green']}
 
-# linearer Fit
-# Fitfunktion definieren
-def fit_function(x, A):
-    return A * x
+for column_name, column in label.items():
+    RF = pd.read_csv(column_name, header=0, sep=';')
 
+    ue = unp.uarray(RF['e'], RF['de'])
+    uI = unp.uarray(RF['I'], RF['dI'])
+    uU_A = unp.uarray(3000, 10) # Anodenspannung
+    KL = u.ufloat(8, 0.05) # Kantenlänge Schirm
 
-# Curve-Fit mit Unsicherheiten in y
-params, covariance = curve_fit(fit_function, x_data, y_data, sigma=y_err, absolute_sigma=True)
-A_value = params[0]
-fit_errors = np.sqrt(np.diag(covariance))  # Fehler der Fit-Parameter
-A_error = fit_errors[0]
+    #aus den Messwerten Magnetfeldstärke berechnen
+    #uB = k*uI
 
-dof = len(RF.index)-len(params)
-chi2 = sum([(fit_function(x,A_value)-y)*2/u*2 for x,y,u in zip(x_data,y_data,y_err)])
+    #aus den Messwerten und der Magnetfeldstärke r(B) berechnen
+    ur = (KL**2+ue**2)/(np.sqrt(2) * (KL-ue))
 
-# Fit-Ergebnisse ausgeben
-#print(f"A = {A_value:.6f} ± {A_error:.6f}")
-#print(f"x0 = {x0_value:.6f} ± {x0_error:.6f}")
-#print(f"Chi-Quadrat/dof: {chi2/dof}")
+    #Daten
+    x_data = RF['I']
+    x_err = RF['dI']
+    y_data = np.array([value.nominal_value for value in ur])
+    y_err = np.array([value.s for value in ur])
 
-x_ax = np.linspace(0, 10, 1000) 
-y_ax = fit_function(x_ax, A_value)
+    #Messwerte plotten
+    ax.errorbar(x_data, y_data, xerr=x_err, yerr=y_err, label=column[0], color=column[2], linestyle='None', marker='o', capsize=6)
 
-# Plot zeichnen
-plt.plot(x_ax, y_ax, label=f"Fit: $y = A \\cdot x$ \n $A = {A_value:.6f} \\pm {A_error:.6f}$", linewidth=2, color='blue')
+    # linearer Fit
+    # Fitfunktion definieren
+    def fit_function(x, A):
+        return A * x
+
+    # Curve-Fit mit Unsicherheiten in y
+    params, covariance = curve_fit(fit_function, x_data, y_data, sigma=y_err, absolute_sigma=True)
+    A_value = params[0]
+    fit_errors = np.sqrt(np.diag(covariance))  # Fehler der Fit-Parameter
+    A_error = fit_errors[0]
+
+    dof = len(RF.index)-len(params)
+    chi2 = sum([(fit_function(x,A_value)-y)*2/u*2 for x,y,u in zip(x_data,y_data,y_err)])
+
+    # Fit-Ergebnisse ausgeben
+    #print(f"A = {A_value:.6f} ± {A_error:.6f}")
+    #print(f"x0 = {x0_value:.6f} ± {x0_error:.6f}")
+    #print(f"Chi-Quadrat/dof: {chi2/dof}")
+
+    x_ax = np.linspace(0, 1, 1000) 
+    y_ax = fit_function(x_ax, A_value)
+
+    column[2] = column[2] + f"$y = A \\cdot x$ \n $A = {A_value:.6f} \\pm {A_error:.6f}$"
+    
+    #
+    # Plot zeichnen
+    plt.plot(x_ax, y_ax, label=column[1], linewidth=2, color=column[3])
 
 plt.xlabel('Stromstärke $I$ in A')
-plt.ylabel('Magnetfeldstärke $B$ in mT)')
+plt.ylabel('Radius $r$ in cm')
 plt.legend() #Legende printen
-plt.title("Magnetfeldstärke $B$ in Abhängigkeit der Stromstärke $I$")
+plt.title("Radius $r$ in Abhängigkeit der Stromstärke $I$")
 
 plt.savefig("E12/B-I-Diagramm.pdf", format='pdf', bbox_inches='tight', pad_inches=0.5) 
 plt.savefig("E12/B-I-Diagramm.svg", format='svg', bbox_inches='tight', pad_inches=0.5) 
