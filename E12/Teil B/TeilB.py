852e380635ee173cc33bc51b16c9891c1849380d
felos259 <felos259@gmail.com>
2025-04-26 19:00:33 +0200

    Dinge zu tun

---
+++ E12/Teil B/TeilB.py
@@ -0,0 +1,11 @@
+import numpy as np  # Numpy array library
+import pandas as pd # Pandas Dataframes um Messdaten zu speichern
+import matplotlib.pyplot as plt # Plots erstellen
+import matplotlib.mlab as mlab
+import scipy.stats # Statistische Auswertung - Unsicherheiten werden automatisch eingerechnet
+from scipy.optimize import curve_fit
+import iminuit as i
+import uncertainties as u
+import uncertainties.umath as um
+from uncertainties import unumpy as unp
+
