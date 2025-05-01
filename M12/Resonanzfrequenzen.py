f482bedf3a13b9605b9a6ab9747177d1b53b45c4
Chisolmc <conradchisolm@gmail.com>
2025-05-01 20:55:36 +0200

    Update Resonanzfrequenzen.py

--- M12/Resonanzfrequenzen.py
+++ M12/Resonanzfrequenzen.py
@@ -99,7 +99,7 @@ for i in range(0,3,1):
     A_error= fit_errors[0]
 
     dof = len(RF.index)-len(params)
-    chi2 = sum([(fit_function(x,A_value)-y)*2/u*2 for x,y,u in zip(x_data,y_data,y_err)])
+    chi2 = sum([((fit_function(x,A_value)-y)**2)/(u**2) for x,y,u in zip(x_data,y_data,y_err)])
 
     # Fit-Ergebnisse ausgeben
     #print(f"A = {A_value:.6f} ± {A_error:.6f}")
