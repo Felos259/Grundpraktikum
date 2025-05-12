512018edd1a353477da8b2a3303af5aa625a9926
felos259 <felos259@gmail.com>
2025-05-12 21:39:53 +0200

    sync with pc

--- E12/Teil A/TeilA.py
+++ E12/Teil A/TeilA.py
@@ -9,41 +9,53 @@ import uncertainties as u
 import uncertainties.umath as um
 from uncertainties import unumpy as unp
 
-# Unscicherheiten anpassen k, uU_A
+# Unscicherheiten anpassen k
 
 mu_0 = 4*np.pi*10**(-7)
 N = 320
 R = 0.068
 
-k = u.ufloat(mu_0 *N/(2*R)*(4/5)**(3/2),0)
+k = mu_0 *N/(2*R)*(4/5)**(3/2)
+
+# Anodenspannungen mit Unsicherheiten
+uU_A = unp.uarray([3000, 4000, 5000], [100, 100, 100]) 
 
 ################################
-
 
 fig, ax = plt.subplots()
 # fig ist das eigentliche Bild, ax ist ein Datenobjeke
 
 # Achsen richten
-ax.set_xlim(0, 7)
-ax.set_ylim(0, 200)
+ax.set_xlim(0, 750)
+ax.set_ylim(0, 1)
 
 # Datenträger, damit man subplots ansprechen kann - für hübsche Legende
 # Labels und Farben für Achsen speichern 
-label = {'E12/Teil A/3kV.csv' : ['$U_A=3$kV', 'Fit zu $U_A=3$kV', 'red', 'orange'],
-         'E12/Teil A/4kV.csv' : ['$U_A=4$kV', 'Fit zu $U_A=4$kV', 'blue', 'purple'],
-         'E12/Teil A/5kV.csv' : ['$U_A=5$kV', 'Fit zu $U_A=5$kV', 'grey', 'green']}
+label = {'E12/Teil A/3kV.csv' : ['$U_A=3$kV', 'Fit zu $U_A=3$kV', 'red', 'coral'],
+         'E12/Teil A/4kV.csv' : ['$U_A=4$kV', 'Fit zu $U_A=4$kV', 'blue', 'lightblue'],
+         'E12/Teil A/5kV.csv' : ['$U_A=5$kV', 'Fit zu $U_A=5$kV', 'green', 'limegreen']}
 
-# Abspeichern der Fit-Wert für Berechnungen von e/m
-uA = []
+# Abspeichern der FIT-Wert für Berechnungen von e/m
+uA = [] 
+
 
 for column_name, column in label.items():
     
     RF = pd.read_csv(column_name, header=0, sep=';')
+    
+    for j in range(0, len(RF['dI']) , 1):
+        # Einheiten in SI umwandeln 
+        RF.loc[j, 'I'] = RF['I'][j] / 1000
+        RF.loc[j, 'dI'] = RF['dI'][j] / 1000
+        RF.loc[j, 'e'] = RF['e'][j] / 1000
+        RF.loc[j, 'de'] = RF['de'][j] / 1000
+        # Unsicherheiten in I Berechnen
+        RF.loc[j,'dI'] = 0.012 * RF['I'][j] + 0.00005
 
     ue = unp.uarray(RF['e'], RF['de'])
     uI = unp.uarray(RF['I'], RF['dI'])
-    uU_A = unp.uarray(3000, 10) # Anodenspannung
-    KL = u.ufloat(8, 0.05) # Kantenlänge Schirm
+    
+    KL = u.ufloat(0.08, 0.0005) # Kantenlänge Schirm
 
     # 1/I für antiproportionalen Zusammenhang berechnen
     UdurchI = 1/uI
@@ -82,10 +94,12 @@ for column_name, column in label.items():
     #print(f"x0 = {x0_value:.6f} ± {x0_error:.6f}")
     #print(f"Chi-Quadrat/dof: {chi2/dof}")
 
-    x_ax = np.linspace(0, 10, 1000) 
+    x_ax = np.linspace(0, 1000, 1000) 
     y_ax = fit_function(x_ax, A_value)
 
-    column[2] = column[2] + f"$y = A \\cdot x$ \n $A = {A_value:.6f} \\pm {A_error:.6f}$"
+
+    column[1] = column[1] + f"$y = A \\cdot x$ \n $A = {A_value:.6f} \\pm {A_error:.6f}$"
+
     uA.append(u.ufloat(A_value, A_error))
     
     # Plot zeichnen
@@ -108,5 +122,5 @@ uA = unp.uarray([value.nominal_value for value in uA],
                 [value.s for value in uA]) 
 
 # e/m berechnen
-ue_m = 2 * uU_A / ((uA * k)**2)
-print(ue_m)
+#ue_m = 2 * uU_A / ((uA * k)**2)
+#print(ue_m)
