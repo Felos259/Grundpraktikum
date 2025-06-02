656f94a4917498350a9f246cf657357d39d44284
Chisolmc <conradchisolm@gmail.com>
2025-06-02 20:47:40 +0200

    Fit-Funktion berechnet Kurzschlussstrom

--- E5 Programmierung/Messaufgabe1.py
+++ E5 Programmierung/Messaufgabe1.py
@@ -59,16 +59,14 @@ y_data_4 = RF['U_2Weg_mitC']*10/3
 y_err_4 = RF['uV2mC']
 
 # Plot der Messwerte V und p mit Errorbars 
-ax.errorbar(x_data_1,y_data_1, xerr=x_err_1 , yerr=y_err_1, label='EWG ohne Kondensator', color = 'lightblue', linestyle='None', marker='o', capsize=6)
-ax.errorbar(x_data_2, y_data_2, xerr=x_err_2 , yerr=y_err_2, label='EWG mit Kondensator', color = 'darkblue', linestyle='None', marker='o', capsize=6)
-ax.errorbar(x_data_3, y_data_3, xerr=x_err_3 , yerr=y_err_3, label='ZWG ohne Kondensator', color = 'lightgreen', linestyle='None', marker='o', capsize=6)
-ax.errorbar(x_data_4, y_data_4, xerr=x_err_4 , yerr=y_err_4, label='ZWG mit Kondensator', color = 'darkgreen', linestyle='None', marker='o', capsize=6)
+ax.errorbar(x_data_1,y_data_1, xerr=x_err_1 , yerr=y_err_1, label='U(I) für E1', color = 'lightblue', linestyle='None', marker='o', capsize=6)
+ax.errorbar(x_data_2, y_data_2, xerr=x_err_2 , yerr=y_err_2, label='U(I) für E2', color = 'darkblue', linestyle='None', marker='o', capsize=6)
+ax.errorbar(x_data_3, y_data_3, xerr=x_err_3 , yerr=y_err_3, label='U(I) für Z1', color = 'lightgreen', linestyle='None', marker='o', capsize=6)
+ax.errorbar(x_data_4, y_data_4, xerr=x_err_4 , yerr=y_err_4, label='U(I) für Z2', color = 'darkgreen', linestyle='None', marker='o', capsize=6)
 
-
-#To-Do: Gemäß theoretischen Ergebnis überarbeiten!
 # Fitfunktion definieren
 def fit_function(x, A, x0):
-    return A * x + x0
+    return A * (x - x0)
 
 
 # Curve-Fit mit für EINWEG ohne Kondensator
@@ -83,14 +81,14 @@ dof_1 = len(RF.index)-len(params_1)
 chi2_1 = sum([((fit_function(x,A_1_value,x0_1_value)-y)**2)/(u**2) for x,y,u in zip(x_data_1,y_data_1,y_err_1)])
 # Fit-Ergebnisse ausgeben
 print(f"1-Weg ohne Kondensator: A = {A_1_value:.6f} ± {A_1_error:.6f} (Innenwiderstand EWG)")
-print(f"1-Weg ohne Kondensator: x0 = {x0_1_value:.6f} ± {x0_1_error:.6f} (Leerlaufspannung EWG)")
-I0_1=-unp.uarray([x0_1_value],[x0_1_error])/(unp.uarray([A_1_value],[A_1_error]))
-print("Kurzschlussstrom EWG: ", I0_1)
+print(f"1-Weg ohne Kondensator: x0 = {x0_1_value:.6f} ± {x0_1_error:.6f} (Kurzschlussstrom EWG)")
+U0_1=-unp.uarray([x0_1_value],[x0_1_error])*(unp.uarray([A_1_value],[A_1_error]))
+print("Leerlaufspannung EWG: ", U0_1)
 print(f"1-Weg ohne Kondensator: Chi-Quadrat/dof: {chi2_1/dof_1}")
 # Fit-Ergebnisse plotten
 x_ax_1=np.linspace(0, 300, 1000) 
 y_ax_1 = fit_function(x_ax_1, A_1_value,x0_1_value)
-plt.plot(x_ax_1, y_ax_1, label=f"EWG-Fit: $y = A \\cdot x+x_0$ \n $A = {A_1_value:.6f} \\pm {A_1_error:.6f}$ \n $x_0 = {x0_1_value:.6f} \\pm {x0_1_error:.6f}$", linewidth=2, color='blue')
+plt.plot(x_ax_1, y_ax_1, label=f"E1-Fit: $y = A \\cdot (x-x_0)$ \n $A = {A_1_value:.6f} \\pm {A_1_error:.6f}$ \n $x_0 = {x0_1_value:.6f} \\pm {x0_1_error:.6f}$", linewidth=2, color='blue')
 
 # Curve-Fit mit für ZWEIWEG ohne Kondensator
 params_3, covariance_3 = curve_fit(fit_function, x_data_3, y_data_3, sigma=y_err_3, absolute_sigma=True)
@@ -104,13 +102,13 @@ dof_3 = len(RF.index)-len(params_3)
 chi2_3 = sum([((fit_function(x,A_3_value,x0_3_error)-y)**2)/(u**2) for x,y,u in zip(x_data_3,y_data_3,y_err_3)])
 # Fit-Ergebnisse ausgeben
 print(f"2-Weg ohne Kondensator: A = {A_3_value:.6f} ± {A_3_error:.6f} (Innenwiderstand ZWG)")
-print(f"2-Weg ohne Kondensator: x0 = {x0_3_value:.6f} ± {x0_3_error:.6f} (Leerlaufspannung ZWG)")
-I0_1=-unp.uarray([x0_3_value],[x0_3_error])/(unp.uarray([A_3_value],[A_3_error]))
-print("Kurzschlussstrom EWG: ", I0_1)
+print(f"2-Weg ohne Kondensator: x0 = {x0_3_value:.6f} ± {x0_3_error:.6f} (Kurzschlussstrom ZWG)")
+U0_3=-unp.uarray([x0_3_value],[x0_3_error])*(unp.uarray([A_3_value],[A_3_error]))
+print("Leerlaufspannung EWG: ", U0_3)
 print(f"2-Weg ohne Kondensator: Chi-Quadrat/dof: {chi2_3/dof_3}")
 x_ax_3=np.linspace(0, 300, 1000) 
 y_ax_3 = fit_function(x_ax_3, A_3_value,x0_3_value)
-plt.plot(x_ax_3, y_ax_3, label=f"ZWG-Fit: $y = A \\cdot x+x_0$ \n $A = {A_3_value:.6f} \\pm {A_3_error:.6f}$ \n $x_0 = {x0_3_value:.6f} \\pm {x0_3_error:.6f}$", linewidth=2, color='limegreen')
+plt.plot(x_ax_3, y_ax_3, label=f"Z1-Fit: $y = A \\cdot (x-x_0)$ \n $A = {A_3_value:.6f} \\pm {A_3_error:.6f}$ \n $x_0 = {x0_3_value:.6f} \\pm {x0_3_error:.6f}$", linewidth=2, color='limegreen')
 
 # Plot zeichnen
 plt.xlabel('Laststrom $I$ in A')
