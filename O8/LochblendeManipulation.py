02826699ae9de3f491555224156dffe62e17edc6
felos259 <felos259@gmail.com>
2025-07-03 21:14:19 +0200

    as best as it gets

--- O8/LochblendeManipulation.py
+++ O8/LochblendeManipulation.py
@@ -5,6 +5,7 @@ import matplotlib.mlab as mlab
 import scipy.stats # Statistische Auswertung - Unsicherheiten werden automatisch eingerechnet
 from scipy.optimize import curve_fit
 from scipy.signal import argrelmax
+from scipy.signal import argrelmin
 from scipy.special import j1
 import iminuit as i
 import uncertainties as u
@@ -21,6 +22,10 @@ plt.rcParams['figure.figsize'] = [19.2,10.8]
 dgs = 4
 lamb = 532 * 10**-6
 SS = u.ufloat(86.5, 0.5) - u.ufloat(2.0, 0.5) 
+# Durchmesser mit Mikroskop
+B = 20 * u.ufloat(4.98, 0.02) * 10**-3
+print('Durchmesser Lochblende Mikroskop: B='+ str(B) + "cm")
+
 
 
 ###############
@@ -91,7 +96,7 @@ for k in range(0, int(length/dgs) , 1):
 
 substancialPeakDemand = int(len(SmoothRF) * 0.025)
 
-local_max_smooth_aggr = argrelmax(SmoothRF['Intensity'].to_numpy(), order = substancialPeakDemand)
+local_max_smooth_aggr = argrelmin(SmoothRF['Intensity'].to_numpy(), order = substancialPeakDemand)
 indexList = np.asarray(local_max_smooth_aggr)[0]
 
 peaks = SmoothRF[SmoothRF.index.isin(indexList)]
@@ -99,8 +104,8 @@ peaks = SmoothRF[SmoothRF.index.isin(indexList)]
 peaks = peaks.sort_values('position')
 
 # PEAKS ENTFERNEN, WEIL SIE CLUTTER SIND
-peaks = peaks.iloc[4:]
-peaks = peaks.iloc[:len(peaks)-4]
+peaks = peaks.iloc[3:]
+peaks = peaks.iloc[:len(peaks)-2]
 
 # Werte abspeichern
 peaks.to_csv('O8/Lochblende.csv', sep=';', index = False)
@@ -109,86 +114,64 @@ peaks.to_csv('O8/Lochblende.csv', sep=';', index = False)
 # Positionen so verschieben, dass Maximum 0. Ordnung bei 0cm liegt
 
 position = position - RF['position'].iloc[peaks.idxmax().iloc[2]*dgs]
-peaks['position'] = peaks['position']- SmoothRF['position'].iloc[peaks.idxmax().iloc[2]]
-SmoothRF['position'] = SmoothRF['position'] - SmoothRF['position'].iloc[peaks.idxmax().iloc[2]]
+
+# posminus1 = peaks.idxmax().iloc[2]
+# print(posminus1)
+ 
+halfpoint = (SmoothRF['position'].iloc[449] + SmoothRF['position'].iloc[607] )  /2
+
+
+peaks['position'] = peaks['position']- halfpoint
+SmoothRF['position'] = SmoothRF['position'] - halfpoint
 
 ###################################################################################################
 # Plot
-
 fig, ax = plt.subplots()
-# fig ist das eigentliche Bild, ax ist ein Datenobjeke
 
+ax.set_ylim(-0.1, 1.4)
+ax.set_xlim(-3.0, 3.0)
 
 # Peaks plotten
 ax.errorbar(peaks['position'],  peaks['Intensity'], 
-        label = "Maxima der geglätteten Daten", 
-        color = 'lightgreen', linestyle='None', marker='o', markersize=8)
- 
-# Smoothed Data plotten
-ax.errorbar(x = SmoothRF['position'], y = SmoothRF['Intensity'], 
-        label = "geglättete Daten - je " + str(dgs) + " Pixel zusammengefasst", 
-        color = 'red', linestyle='None', marker='o', markersize=2, capsize=3, elinewidth = 0.5)
-# xerr = SmoothRF['dPos'], yerr = SmoothRF['dInt'],
+        label = "Minima der geglätteten Daten", 
+        color = 'mediumblue', linestyle='None', marker='o', markersize=8)
+
 
 #Messwerte plotten
 # ax.errorbar(np.array([value.n for value in position]),  Intensity, label = 'Intensität des Lichtes Lochblende', 
 #             color = 'mediumblue', linestyle='None', marker='o', capsize=3, markersize=1, elinewidth = 0.5)
 
+#Nullstellen Bessel
+NS = [-19.6159, -16.471, -13.324, -10.173, -7.016, -3.832, 0.0, 3.832, 7.016, 10.173, 13.324, 16.471, 19.6159]
 
+#Theoretische Minimapositionen
+posMin = [value * lamb * SS / (np.pi * B) for value in NS]
 
-##########################
-# PloFit für Hälfte der Maxima durchführen
-# start = (peaks.idxmax()[2]+1)
+I_0 = 1.0
 
-x_data = SmoothRF['position']#[start:]
-x_err = SmoothRF['dPos']#[start:]
-y_data = SmoothRF['Intensity']#[start:]
-y_err = SmoothRF['dInt']#[start:]
+def theoFunkt(x, I):
+    return I * (j1( np.pi * B.n * x / (SS.n*lamb)) / (np.pi * B.n * x / (2.0*SS.n*lamb)) )**2
 
+x_ax = np.linspace(-3, 3, 7000) 
+y_ax = theoFunkt(x_ax, I_0)
 
-# # Fitfunktion definieren
-#         # param B = Lochblenden durchmessen
-#         # Kleiwinkelnäherung sin(alp)=tan(alp)= pos/SS = x/SS
-# def fit_function(x, I, B):
-#     return  I * (j1(1, np.pi*B*x / (lamb*SS.n)) / (np.pi*B*x / (lamb*2.0*SS.n)))**2
+ax.errorbar(x = x_ax , y = y_ax,
+        label = "theoretische Funktion mit B aus dem Mikroskop", color = 'plum')
 
+ax.errorbar(x = [value.n for value in posMin] , y = [theoFunkt(value.n, I_0) for value in posMin], xerr = [value.s for value in posMin], 
+        label = "Nullstellen der theoretischen Funktion ", 
+        color = 'purple', linestyle='None', marker='o', markersize=5, capsize=6)
 
-# # Curve-Fit mit Unsicherheiten in y
-# params, covariance = curve_fit(fit_function, x_data, y_data, sigma = y_err) # 
-# fit_errors = np.sqrt(np.diag(covariance))  # Fehler der Fit-Parameter
-# I_value = params[0]
-# I_error = fit_errors[0]
-# B_value = params[1]
-# B_error = fit_errors[1]
-
-# dof = len(RF.index)-len(params)
-# # chi2 = sum([((fit_function(x,I_value, B_value)-y)**2)/(u**2) for x,y,u in zip(x_data,y_data,y_err)])
-
-# # Fit-Ergebnisse ausgeben
-# # print(f"A = {A_value:.6f} ± {A_error:.6f}")
-# # print(f"x0 = {x0_value:.6f} ± {x0_error:.6f}")
-# # print(f"Chi-Quadrat/dof: {chi2/dof}")
-
-# x_ax = np.linspace(-5, 5, 1000) 
-# y_ax = fit_function(x_ax, I_value, B_value)
-
-
-# # D ist Abstand SS zwischen Schrim und Lochblende
-# #label = r'Fit: $I = \left( \frac{J_1(\theta \\ 2)}{\theta \\ 4}\right)^2$ mit $\theta = \frac{2 \pi \cdot B \cdot x}{\lambda \cdot D}$'  
-# label = r'Fit: $I = \left( \frac{J_1(A \cdot x)}{ \frac{A \cdot x}{2} }\right)^2$ mit $\theta = \frac{2 \pi \cdot B \cdot x}{\lambda \cdot D}$'  
-# label += f"\n $(I = {I_value:.6f} \\pm {I_error:.6f})$ cm"
-# label += f"\n $(A = {B_value:.6f} \\pm {B_error:.6f})$ cm"
-
-
-# Plot zeichnen
-#plt.plot(x_ax, y_ax, label = label, linewidth = 2, color = 'lightblue')
-
-
+ 
+# Smoothed Data plotten
+ax.errorbar(x = SmoothRF['position'], y = SmoothRF['Intensity'], 
+        label = "geglättete Daten - je " + str(dgs) + " Pixel zusammengefasst", 
+        color = 'crimson', linestyle='None', marker='o', markersize=2, capsize=3, elinewidth = 0.5)
+# xerr = SmoothRF['dPos'], yerr = SmoothRF['dInt'],
 
 ################
 
 # cosmetics
-
 plt.xlabel('Position $x$ in cm',fontsize=fnt)
 plt.ylabel('Intensität in % des maximalen Grauwertes', fontsize=fnt)
 plt.legend(fontsize=fnt, loc='upper left') #Legende printen
